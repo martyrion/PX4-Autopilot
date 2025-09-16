947dbbab0fe9c2f937110c1e8363965474ce5447
Your Name <your.email@example.com>
2025-09-16 21:47:38 +0300

    Included the script for auto-generation of param macros

---
+++ platforms/common/include/px4_platform_common/param_generator.py
@@ -0,0 +1,31 @@
+#! /bin/python
+# generate a C/C++ macro APPLY_ALL up to 'count' arguments
+import sys
+
+count = 180
+
+def output(s):
+    sys.stdout.write(s)
+
+def var_name(i):
+    b = ord('a')
+    return chr(b + ((i // 26 // 26) % 26)) \
+         + chr(b + ((i // 26) % 26)) \
+         + chr(b + (i % 26))
+
+for i in range(count):
+    output("#define APPLY{:}(t".format(i))
+    output(''.join([", {:}".format(var_name(j)) for j in range(i)]))
+    output(") ")
+    output(''.join(["t({:}) ".format(var_name(j)) for j in range(i)]))
+    output("\n")
+
+output('''\n#define NUM_ARGS_H1(dummy{:}, ...) x0
+'''.format(''.join([', x'+str(x) for x in reversed(range(count))])))
+output('''#define NUM_ARGS(...) NUM_ARGS_H1(dummy, ##__VA_ARGS__{:})
+'''.format(''.join([', '+str(x) for x in reversed(range(count))])))
+output('''
+#define APPLY_ALL_H3(t, n, ...) APPLY##n(t, __VA_ARGS__)
+#define APPLY_ALL_H2(t, n, ...) APPLY_ALL_H3(t, n, __VA_ARGS__)
+#define APPLY_ALL(t, ...) APPLY_ALL_H2(t, NUM_ARGS(__VA_ARGS__), __VA_ARGS__)
+''')
